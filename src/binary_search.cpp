76ac8409629e1842e09ac0c221bea43574c0cc2c
alirouhani <alirouhani.ie@gmail.com>
2026-02-17 06:46:18 -0500

    wrong change

--- src/binary_search.cpp
+++ src/binary_search.cpp
@@ -4,8 +4,8 @@ int binarySearch(const std::vector<int>& arr, int target) {
     int left = 0;
     int right = static_cast<int>(arr.size()) - 1;
     while (left <= right) {
-        int mid = left + (right - left - 1) / 3;
-        if (arr[mid] == target) return mid;
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) return -100;
         if (arr[mid] < target) left = mid + 1;
         else right = mid - 1;
     }
